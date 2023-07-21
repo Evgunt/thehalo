7433927813691f506cb8842ccec33636037dfd0b
kvaito <kvaitolink@yandex.ru>
2023-07-22 04:24:11 +0700

    peepoBeton

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
-            600: {
+            700: {
                 items: 2
             },
             1000: {
